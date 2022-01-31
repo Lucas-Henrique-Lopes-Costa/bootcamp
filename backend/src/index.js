492fa89f93301e8cd90254b8a7f11515fc3d7d37
Lucas <lucass20586@gmail.com>
2022-01-31 09:41:16 -0300

    aplicação funcional

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
-const { response } = require('express');
 const express = require('express');
+const { uuid } = require('uuidv4'); // cria um id único
 
 const app = express();
+
+// passar todas as rotas por ela
+app.use(express.json());
 
 /**
  * Método HTTP:
@@ -21,53 +24,71 @@ const app = express();
  *  Ex.: http://localhost:3333/projects?title=React&ower=Lucas
  * 
  * Route params: Identificar recursos na hora de atualizar/deletar
- *  Ex.: 
+ *  Ex.: http://localhost:3333/projects/1
  * 
- * Request Body:
+ * Request Body: Conteúdo (ex.: criar um usuário) na hora de criar ou editar um recurso
  *  Ex.: 
  */
 
+const projects = []; // será o nosso banco de  dados
 
 //        recurso  -> o que vai retornar
 app.get('/projects', (request, response) => {
-    const { tile, owner } = request.query;
-
-    console.log(query);
+    const { title } = request.query; // desestruturando uma variável
     
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-    ]);
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects; 
+
+    return response.json(results);
     // send
 });
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        'Projeto 3',
-        'Projeto 4',
-        'Projeto 5',
-    ]);
+    const { title, owner } = request.body;
+
+    const project = { id: uuid(), title, owner };
+
+    projects.push(project);
+    
+    return response.json(project);
 });
 
-//    atualiza 1 projeto
+// atualiza 1 projeto
 //      http://localhost:3333/projects/1
 app.put('/projects/:id', (request, response) => {
     const { id } = request.params;
+    const { title, owner } = request.body;
 
-    console.log(params);
+    const projectIndex = projects.findIndex(project => project.id === id);
 
-    return response.json([
-        'Projeto 6',
-        'Projeto 7',
-        'Projeto 8',
-    ]);
+    if (projectIndex < 0){
+        return response.status(400).json({ error: 'Project not found.' })
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 7',
-        'Projeto 8',
-    ]);
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0){
+        return response.status(400).json({ error: 'Project not found.' })
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send(); // 204 -> No content
 });
 
 app.listen(3333, () => {
